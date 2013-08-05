a2df94cd2a852bb92875dc680ef21120e9a988ad
David Cook <david@redcliffs.net>
2013-08-05 11:03:31 +1000

    Replace number_to_currency with Spree::Money.new (as per https://github.com/spree/spree/pull/1829)

--- app/helpers/spree/orders_helper.rb
+++ app/helpers/spree/orders_helper.rb
@@ -8,7 +8,7 @@ module Spree
     def order_delivery_fee_subtotal(order, options={})
       options.reverse_merge! :format_as_currency => true
       amount = order.line_items.map { |li| li.itemwise_shipping_cost }.sum
-      options.delete(:format_as_currency) ? number_to_currency(amount) : amount
+      options.delete(:format_as_currency) ? Spree::Money.new(amount) : amount
     end
 
     def alternative_available_distributors(order)
